6de0ed7a322d07ed17d85e551e95afa057f823c7
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-26 03:19:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,20 @@
 def my_collect(array)
   counter = 0
+  new_array = []
   while counter < array.length
-    yield(array[i])
+    new_array.push(yield(array[counter]))
     counter += 1
   end
+  new_array
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
 my_collect(array) do |name|
   name.split('').first
+end
+
+my_collect(collection) do |language|
+  language.upcase
 end
